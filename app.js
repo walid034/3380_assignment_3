c8662051d703d8e2e099344ed9116e98c99832ea
Walid Amin Khan <walid.sec3.034@gmail.com>
2023-06-05 22:43:42 -0700

    Added assignment files

---
+++ app.js
@@ -0,0 +1,57 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false}));
+
+// // code here for adding static assets
+// app.use('/static', express.static('/public'));
+
+// app.set('view engine', 'pug');
+
+// app.get('/', (req, res) => {
+//   res.render('bmi');
+// });
+
+// app.post('/', (req, res) => {
+//   res.render('bmi', { name: req.body.username , colors: colors });
+// });
+
+// app.listen(3000, () => {
+//     console.log('The application is running on localhost:3000!')
+// });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// code here for adding static assets
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('bmi');
+});
+
+app.post('/', (req, res) => {
+  var age = req.body.age;
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height)/100;
+  var bmi = weight/(height**2);
+  if (isNaN(bmi)){
+    res.render('bmi', { result: "Height and weight must be numbers"});
+  }
+  else {
+    res.render('bmi', { result: "Your BMI is: "+bmi.toString()});
+  }
+
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
